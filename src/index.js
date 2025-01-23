458931169664100c70df31c37c21f3dc81f311c5
Samuel Castro <samuglez90@gmail.com>
2025-01-23 15:40:53 +0000

    Hecho Samuel

--- src/index.js
+++ src/index.js
@@ -7,32 +7,46 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+const startButton = document.getElementById("start-btn");
+startButton.onclick = startCountdown;
+// startButton.addEventListener("Click", startCountdown);
 
 
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  let timeRemaining = 10;
+  const startButton = document.getElementById("start-btn");
+  startButton.disabled = true;
+  const timeElement = document.getElementById("time");
+  const intervalId = setInterval(function() {
+    timeRemaining = timeRemaining -1;
+    timeElement.textContent = timeRemaining;
+    if (timeRemaining === 0) {
+      clearInterval(intervalId);
+      showToast();
+      startButton.disabled = false;
+  }
+}, 1000);
 }
+        
+
+
+
+
 
 
 
 
 // ITERATION 3: Show Toast
-function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
-
+function showToast() {
+  const toast = document.getElementById("toast");
+  toast.classList.add("show");
+  setTimeout(function () {
+      toast.classList.remove("show");
+  }, 3000);
+}
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
-}
